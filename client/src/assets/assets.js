ec5779cea0f71c3717435f5ea0bb4fb12e1f85a4
Pathsharasakon <pathsharasakon@gmail.com>
2026-08-29 09:30:45 +0700

    feature: improve shared navbar interactions

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -4,10 +4,20 @@ import cartBag from "./icon/cartBag.png";
 import line from "./icon/line.png";
 import userimg from "./icon/user.png";
 import down from "./icon/down.png";
+import facebook from "./icon/facebook.png";
+import instagram from "./icon/instagram.png";
+import lineOfficial from "./icon/line.png";
+import logout from "./icon/logout.png";
+import tiktok from "./icon/tiktok.png";
 export const assets = {
   newlogo,
   search,
   cartBag,
   userimg,
   down,
+  facebook,
+  instagram,
+  lineOfficial,
+  logout,
+  tiktok,
 };
