8413fabd5b4f1c232a708a04281d18e86c432bc6
Puttipong Runtakit <puttipong.runtakit@gmail.com>
2026-09-05 11:23:19 +0700

    update homepage

--- client/src/pages/RecommendProduct.jsx
+++ client/src/pages/RecommendProduct.jsx
@@ -6,12 +6,14 @@ export const RecommendProduct = ({ product, index }) => {
     >
       <a href="Product_Page.html?category=tops" className="block h-full w-full">
         <img
-          src={product.image}
-          alt={product.title}
+          src={product.image || product.items?.[0]?.image}
+          alt={product.title || product.nameTh || product.name}
           className="h-full w-full cursor-pointer object-cover transition duration-300 ease-in-out group-hover:scale-105"
         />
         <div className="absolute bottom-4 left-4 font-bold text-white drop-shadow-lg">
-          <h4 className="text-3xl">{product.title}</h4>
+          <h4 className="text-3xl">
+            {product.title || product.nameTh || product.name}
+          </h4>
           <h6 className="text-sm font-normal opacity-90">สำรวจหมวดหมู่</h6>
         </div>
       </a>
