14a2171b6860d9b0c769553e1c86848e1fc9ea7e
Bell914 <ittikorn.tipson@gmail.com>
2026-09-17 16:48:44 +0700

    update services/api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
+  import.meta.env.VITE_API_BASE_URL || "https://jsd13-group3-hydraranger.onrender.com/api";
 
 class ApiClient {
   constructor(baseUrl) {
