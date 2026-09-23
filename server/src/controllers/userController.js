ed127ab7dca448af32c22b0bb9cf365af3b49cbc
Bell914 <ittikorn.tipson@gmail.com>
2026-09-23 14:56:56 +0700

    remove duplicate User import in userController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,7 +1,6 @@
 import { HTTP_STATUS } from '../config/constants.js';
 import { User } from '../models/User.js';
 import * as userService from '../services/userService.js';
-import { User } from '../models/User.js';
 
 export const getUsers = async (req, res, next) => {
   try {
